0dc0de9c8995ef549c0580b0e103c6f0cc45346c
Josh Bain <joshuambain@gmail.com>
2024-07-16 16:13:24 -0400

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+//Instruction 1 solution
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+
+// Instruction 2 solution
+let random = parseInt(Math.random()*10)
+
+// Instruction 3 solution
+const num3 = 24
+const num4 = 5
+let mod = num3 % num4
+
+//Instruction 4 solution
+
+let max = Math.max(1,5,10,20)
